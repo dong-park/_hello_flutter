ef1d40f77e27e18819638d0c83f16e1c5ccb3a0f
dongpark <PARK_DONGHWAN01@eland.co.kr>
2022-11-27 23:34:57 +0900

    add main

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/screen/randomnum_screen.dart';
+import 'package:hello_world/screen/time_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeRandomScreen()),
+  );
+}
