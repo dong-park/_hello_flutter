892ac9b2dc750e54d87751c686e232f237e2af0f
dongpark <PARK_DONGHWAN01@eland.co.kr>
2022-12-10 18:20:15 +0900

    알고 있으면 좋은 push 메소드들

--- lib/screen/route_two_screen.dart
+++ lib/screen/route_two_screen.dart
@@ -7,6 +7,42 @@ class RouteTwoScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MainLayout(title: "routeTwo", routes: []);
+    return MainLayout(title: "routeTwo", routes: [
+      ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).pushReplacement( // 이전에 있던 화면을 지우고 새로운 화면을 띄운다.
+            MaterialPageRoute(
+              builder: (context) => RouteTwoScreen(),
+              settings: RouteSettings(
+                arguments: "two",
+              ),
+            ),
+          );
+        },
+        child: Text('pushReplacement'),
+      ),
+      ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).pushAndRemoveUntil( // 이전에 있던 화면을 지우고 새로운 화면을 띄운다.
+            MaterialPageRoute(
+              builder: (context) => RouteTwoScreen(),
+              settings: RouteSettings(
+                arguments: "two",
+              ),
+            ),
+            // (route) => false, // 이전에 있던 화면을 모두 지운다.
+            // (route) => true, // 이전에 있던 화면을 모두 지우지 않는다.
+            (route) => route.settings.name == '/', // 홈스크린만 남기고 모두 지운다.
+          );
+        },
+        child: Text('push And Remove Until'),
+      ),
+      ElevatedButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Text('Back'),
+      ),
+    ]);
   }
 }
