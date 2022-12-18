af2869d89fd702e3d1c4df23ccb4040ef4a0179f
dong-park <rakddh@gmail.com>
2022-12-18 20:11:16 +0900

    [캘린더스케줄러] 플로팅 액션 스타일 처리

--- lib/component/scheduled_bottom_sheet.dart
+++ lib/component/scheduled_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/constent/color.dart';
 
 class ScheduledBottomSheet extends StatelessWidget {
   const ScheduledBottomSheet({Key? key}) : super(key: key);
@@ -10,15 +11,98 @@ class ScheduledBottomSheet extends StatelessWidget {
     return Container(
       height: MediaQuery.of(context).size.height * 0.5 + bottomInset,
       child: Padding(
-        padding: EdgeInsets.only(bottom: bottomInset),
-        child: Column(children: [
-          TextField(
-            decoration: InputDecoration(
-              hintText: 'Title',
-            ),
+        padding: EdgeInsets.fromLTRB(15, 15, 15, 15 + bottomInset),
+        child:
+            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Row(
+            children: [
+              Expanded(child: CustomTextField(label: '시작시간')),
+              SizedBox(
+                width: 16,
+              ),
+              Expanded(child: CustomTextField(label: '마감시간')),
+            ],
           ),
+          SizedBox(
+            height: 10,
+          ),
+          CustomTextField(label: '내용'),
+          SizedBox(
+            height: 10,
+          ),
+          _ColorPicker(),SizedBox(height: 10,),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('저장'),
+            style: ElevatedButton.styleFrom(backgroundColor: PRIMARY_COLOR),
+          )
         ]),
       ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+
+  const CustomTextField({Key? key, required this.label}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(color: PRIMARY_COLOR, fontWeight: FontWeight.w500),
+        ),
+        TextField(
+          cursorColor: Colors.grey,
+          decoration: InputDecoration(
+              border: InputBorder.none, fillColor: Colors.grey, filled: true),
+        ),
+      ],
+    );
+  }
+}
+
+class _ColorPicker extends StatelessWidget {
+  const _ColorPicker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Wrap(
+        alignment: WrapAlignment.start,
+        crossAxisAlignment: WrapCrossAlignment.center,
+        spacing: 5,
+        children: [
+          _ColorPickerItem(color: Colors.red),
+          _ColorPickerItem(color: Colors.blue),
+          _ColorPickerItem(color: Colors.green),
+          _ColorPickerItem(color: Colors.yellow),
+          _ColorPickerItem(color: Colors.purple),
+          _ColorPickerItem(color: Colors.orange),
+          _ColorPickerItem(color: Colors.pink),
+          _ColorPickerItem(color: Colors.brown),
+          _ColorPickerItem(color: Colors.black),
+        ],
+      ),
+    );
+  }
+}
+
+class _ColorPickerItem extends StatelessWidget {
+  final Color color;
+
+  const _ColorPickerItem({Key? key, required this.color}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 25,
+      height: 25,
+      decoration: BoxDecoration(shape: BoxShape.circle, color: color),
     );
   }
 }
